711c7517424a0ba615858b134778322b9b8740aa
Bervatov <DarckProyect8@gmail.com>
2025-05-17 17:00:47 -0500

    data

--- utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def load_data():
-    recetas = pd.read_csv('data/recetas.csv')
-    alimentos = pd.read_csv('data/alimentos.csv')
-    ejercicios = pd.read_csv('data/ejercicios.csv')
-    return recetas, alimentos, ejercicios
